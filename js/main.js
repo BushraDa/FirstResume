472466c413ae7ec0e31a51f6feffed2d19607b04
BushraDa <boushra.m.da@gmail.com>
2019-08-11 04:08:46 +0300

    adding mixitup tool

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+  
+  // Start mixitup
+  $('#mixing').mixItUp( {
+  		controls: {
+        	toggleLogic: 'and'
+    	},
+    	animation: {
+		    effects: 'fade rotateZ(-90deg)', /* fade scale */
+		    duration: 1000 /* 600 */
+	  	}
+  });
+
+});
